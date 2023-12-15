a3ea4f09492d82dbacc819e9c363b0603387a2bc
MAYBE134E55 <131408128+MAYBE134E55@users.noreply.github.com>
2023-12-15 22:27:12 +0200

    Update db_connection.php.php

--- db_connection.php.php
+++ db_connection.php.php
@@ -8,3 +8,18 @@ try {
     die();
 }
 ?>
+<?php
+// توصيل بقاعدة البيانات
+$host = "github.com";
+$dbname = "mongodb+srv://publicgeneral:publicgeneral@public.a85ijsx.mongodb.net/";
+$username = "fhad&RIO";
+$password = "FHAD&RIO VIP";
+
+try {
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo "فشل الاتصال بقاعدة البيانات: " . $e->getMessage();
+    die();
+}
+?>
